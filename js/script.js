c5e960e3733a38e72bccef165b56f7d25c4e5f95
Victor Jasin <vjasin@gmail.com>
2024-05-20 21:36:18 -0300

    Actualizo Carousel animado no automático

--- js/script.js
+++ js/script.js
@@ -1,62 +1,33 @@
+let slideIndex = 1;
+mostrarSlides(slideIndex);
 
-let carousel = document.querySelector("#carousel");
-let itemsCarousel = document.querySelectorAll(".items");
-let itemCarouselLast = itemsCarousel[itemsCarousel.length - 1];
-
-const botonIzquierdo = document.querySelector("#izquierdo");
-const botonDerecho = document.querySelector("#derecho");
-
-carousel.insertAdjacentElement('afterbegin', itemCarouselLast);
-
-// function moverDerecha(){
-//     let itemCarouselFirst = document.querySelectorAll(".items")[0];    
-//     carousel.insertAdjacentElement('beforeend', itemCarouselFirst);
-//     carousel.style.transition = "all 0.5s ease";
-// }
-
-function moverDerecha(){
-    let itemCarouselFirst = document.querySelectorAll('.items')[0];
-    carousel.style.marginleft = "-200%";
-    carousel.style.transition = "all 0.5s linear";
-    carousel.insertAdjacentElement('beforeend', itemCarouselFirst);
-    carousel.style.marginleft = "-100";
-    // setTimeout (function(){
-    //     // carousel.style.transition = "none";
-    //     carousel.insertAdjacentElement('beforeend', itemCarouselFirst);
-    //     carousel.style.marginleft = "-100";
-    // }, 500);
+// Next/previous controls
+function desplazar(n) {
+  mostrarSlides(slideIndex += n);
 }
 
-// function moverIzquierda(){
-//     let itemCarousel = document.querySelectorAll(".items");    
-//     let itemCarouselLast = itemCarousel[itemCarousel.length-1];
-//     carousel.insertAdjacentElement('afterbegin', itemCarouselLast);
-//     carousel.style.transition = "all 0.5s ease";
-// }
-
-function moverIzquierda(){
-    let itemCarousel = document.querySelectorAll(".items");
-    let itemCarouselLast = itemCarousel[itemCarousel.length-1];
-    carousel.style.marginleft="0";
-    carousel.style.transition="all 0.5s ease";
-    setTimeout(function(){
-        // carousel.style.transition="none";
-        carousel.insertAdjacentElement('afterbegin', itemCarouselLast);
-        carousel.style.marginleft="-100%";
-    }, 500);
+// Thumbnail image controls
+function actualizar(n) {
+  mostrarSlides(slideIndex = n);
 }
 
-botonDerecho.addEventListener('click', function(){
-    moverDerecha();
-});
-
-botonIzquierdo.addEventListener('click', function(){
-    moverIzquierda();
-});
-
-setInterval(() => moverDerecha(), 3000);
+function mostrarSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("oculta");
+  let dots = document.getElementsByClassName("punto");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
 function enfocarBusqueda(){
     var input = document.getElementById("searchInput");
     input.focus();// Dar foco al input
-}
+};
